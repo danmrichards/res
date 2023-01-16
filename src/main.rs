3a82ca5ed416a494ba03ea94e79244e1589e6117
Dan Richards <dan.m.richards@gmail.com>
2023-01-16 20:13:29 +0000

    Fixes typo in frame module. Remove dead code from main

--- src/main.rs
+++ src/main.rs
@@ -7,91 +7,15 @@ pub mod instructions;
 pub mod ppu;
 pub mod render;
 pub mod tile;
-pub mod trace;
 
 use bus::Bus;
 use cartridge::Rom;
-use cpu::Memory;
 use cpu::CPU;
 use ppu::NESPPU;
 use render::frame::Frame;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
-use sdl2::pixels::Color;
 use sdl2::pixels::PixelFormatEnum;
-use sdl2::EventPump;
-use trace::trace;
-
-const RNG: u16 = 0xFE;
-const LAST_PRESSED: u16 = 0xFF;
-
-fn color(byte: u8) -> Color {
-    match byte {
-        0 => Color::BLACK,
-        1 => Color::WHITE,
-        2 | 9 => Color::GREY,
-        3 | 10 => Color::RED,
-        4 | 11 => Color::GREEN,
-        5 | 12 => Color::BLUE,
-        6 | 13 => Color::MAGENTA,
-        7 | 14 => Color::YELLOW,
-        _ => Color::CYAN,
-    }
-}
-
-fn handle_user_input(cpu: &mut CPU, event_pump: &mut EventPump) {
-    for event in event_pump.poll_iter() {
-        match event {
-            Event::Quit { .. }
-            | Event::KeyDown {
-                keycode: Some(Keycode::Escape),
-                ..
-            } => std::process::exit(0),
-            Event::KeyDown {
-                keycode: Some(Keycode::W),
-                ..
-            } => {
-                cpu.mem_write_byte(LAST_PRESSED, 0x77);
-            }
-            Event::KeyDown {
-                keycode: Some(Keycode::S),
-                ..
-            } => {
-                cpu.mem_write_byte(LAST_PRESSED, 0x73);
-            }
-            Event::KeyDown {
-                keycode: Some(Keycode::A),
-                ..
-            } => {
-                cpu.mem_write_byte(LAST_PRESSED, 0x61);
-            }
-            Event::KeyDown {
-                keycode: Some(Keycode::D),
-                ..
-            } => {
-                cpu.mem_write_byte(LAST_PRESSED, 0x64);
-            }
-            _ => {}
-        }
-    }
-}
-
-fn read_screen_state(cpu: &mut CPU, frame: &mut [u8; 32 * 3 * 32]) -> bool {
-    let mut frame_idx = 0;
-    let mut update = false;
-    for i in 0x0200..0x600 {
-        let color_idx = cpu.mem_read_byte(i as u16);
-        let (b1, b2, b3) = color(color_idx).rgb();
-        if frame[frame_idx] != b1 || frame[frame_idx + 1] != b2 || frame[frame_idx + 2] != b3 {
-            frame[frame_idx] = b1;
-            frame[frame_idx + 1] = b2;
-            frame[frame_idx + 2] = b3;
-            update = true;
-        }
-        frame_idx += 3;
-    }
-    update
-}
 
 fn main() {
     // Initialise SDL.
@@ -140,7 +64,4 @@ fn main() {
 
     cpu.reset();
     cpu.run();
-    // cpu.run_with_callback(move |cpu| {
-    //     println!("{}", trace(cpu));
-    // });
 }
--- src/render/frame.rs
+++ src/render/frame.rs
@@ -4,11 +4,11 @@ pub struct Frame {
 
 impl Frame {
     const WIDTH: usize = 256;
-    const HIGHT: usize = 240;
+    const HEIGHT: usize = 240;
 
     pub fn new() -> Self {
         Frame {
-            data: vec![0; (Frame::WIDTH) * (Frame::HIGHT) * 3],
+            data: vec![0; (Frame::WIDTH) * (Frame::HEIGHT) * 3],
         }
     }
 
